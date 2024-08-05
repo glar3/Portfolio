62b24e113d5cc5bbe7cb8d42dc988f8ca7b02050
glar3 <112261370+glar3@users.noreply.github.com>
2024-08-05 23:02:25 +0200

    Update imports.js

--- src/Portfolio/imports.js
+++ src/Portfolio/imports.js
@@ -1,6 +1,6 @@
 import PortfolioImage from "./Projects/Portfolio.png";
 import WeatherImage from "./Projects/Weather.png";
-import ToDoListImage from "./Projects/toDoList.png";
+import Calendar from "./Projects/Calendar.png";
 import RandomQuoteGeneratorImage from "./Projects/quoteGenerator.png";
 import BMICalculatorImage from "./Projects/BMI.png";
 import PianoStoreImage from "./Projects/piano.png";
@@ -8,7 +8,7 @@ import PianoStoreImage from "./Projects/piano.png";
 export {
   PortfolioImage,
   WeatherImage,
-  ToDoListImage,
+  Calendar,
   RandomQuoteGeneratorImage,
   BMICalculatorImage,
   PianoStoreImage,
